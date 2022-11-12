638ec06a6d7ef1b9e1d667e82501e5bd913b7873
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-12 16:22:29 +0300

    Add flashcard dto

---
+++ FlashCardApp/DTO/FlashCardDTO.cs
@@ -0,0 +1,8 @@
+namespace FlashCardApp.DTO;
+
+public class FlashCardDTO
+{
+    public string Name { get; set; }
+    public string FrontContent { get; set; }
+    public string BackContent { get; set; }
+}
