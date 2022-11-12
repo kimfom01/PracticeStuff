c94ecd5055794d931ad9adbeb3b8673d086b89f5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-12 16:19:00 +0300

    Add flashcard model

---
+++ FlashCardApp/Models/FlashCard.cs
@@ -0,0 +1,10 @@
+namespace FlashCardApp.Models;
+
+public class FlashCard
+{
+    public int Id { get; set; }
+    public int StackId { get; set; }
+    public string Name { get; set; }
+    public string FrontContent { get; set; }
+    public string BackContent { get; set; }
+}
