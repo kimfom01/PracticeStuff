1ff5582d38f809bafcc2dc2e13b9ca33aca37cbc
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-14 23:24:16 +0300

    Run some basic operations to test DBManagerClass

--- FlashCardApp/Program.cs
+++ FlashCardApp/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using FlashCardApp.Data;
+
+DBManager.CreateStackTable();
+DBManager.CreateFlashCardTable();
+DBManager.CreateStudyAreaTable();
+
+DBManager.DeleteStack("States");
+
+DBManager.AddNewStack("Capitals");
+
+PrintStacks();
+
+DBManager.UpdateStack("Capitals", "States");
+
+PrintStacks();
+
+void PrintStacks()
+{
+    var stacks = DBManager.GetStacks();
+
+    foreach (var stack in stacks)
+    {
+        Console.WriteLine(stack.StackName);
+    }
+}
