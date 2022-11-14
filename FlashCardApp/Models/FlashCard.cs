9f224e05f62a2127801ec6ca082bda4bcdd6ceb8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-14 20:13:08 +0300

    Give FlashCards and Stacks approriate names

--- FlashCardApp/Models/FlashCard.cs
+++ FlashCardApp/Models/FlashCard.cs
@@ -4,7 +4,7 @@ public class FlashCard
 {
     public int Id { get; set; }
     public int StackId { get; set; }
-    public string Name { get; set; }
+    public string FlashCardName { get; set; }
     public string FrontContent { get; set; }
     public string BackContent { get; set; }
 }
--- FlashCardApp/Models/Stack.cs
+++ FlashCardApp/Models/Stack.cs
@@ -3,5 +3,5 @@ namespace FlashCardApp.Models;
 public class Stack
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string StackName { get; set; }
 }
