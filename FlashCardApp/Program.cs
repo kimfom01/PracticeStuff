75a69de0fda8172b6d3ca5079b692e4f7197bee7
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-17 10:44:29 +0300

    Clear the Program.cs and call the start program method

--- FlashCardApp/Program.cs
+++ FlashCardApp/Program.cs
@@ -1,27 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using FlashCardApp.UI;
 
-using FlashCardApp.Data;
-
-DBManager.CreateStackTable();
-DBManager.CreateFlashCardTable();
-DBManager.CreateStudyAreaTable();
-
-DBManager.DeleteStack("States");
-
-DBManager.AddNewStack("Capitals");
-
-PrintStacks();
-
-DBManager.UpdateStack("Capitals", "States");
-
-PrintStacks();
-
-void PrintStacks()
-{
-    var stacks = DBManager.GetStacks();
-
-    foreach (var stack in stacks)
-    {
-        Console.WriteLine(stack.StackName);
-    }
-}
+ProgramController.StartProgram();
