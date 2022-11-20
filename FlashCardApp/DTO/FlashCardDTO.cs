2d1b2227a60fa2b22a968885e831f93cc069c6c9
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-20 17:47:50 +0300

    Rename FlashCardDTO properties for better readability

--- FlashCardApp/DTO/FlashCardDTO.cs
+++ FlashCardApp/DTO/FlashCardDTO.cs
@@ -3,6 +3,5 @@ namespace FlashCardApp.DTO;
 public class FlashCardDTO
 {
     public string Name { get; set; }
-    public string FrontContent { get; set; }
-    public string BackContent { get; set; }
+    public string Content { get; set; }
 }
