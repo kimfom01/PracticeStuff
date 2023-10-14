f6853a11941eb0da9d9d2794991122a290f4293f
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-10-14 03:33:27 +0300

    Inject dependencies from constructor

--- FlashCardApp/UI/TableVisualizationEngine.cs
+++ FlashCardApp/UI/TableVisualizationEngine.cs
@@ -6,36 +6,48 @@ namespace FlashCardApp.UI;
 
 public class TableVisualizationEngine
 {
-    private static readonly IDatabaseManager DbManager = new SqlServerDatabaseManager();
+    private readonly IFlashCardDataManager _flashCardDataManager;
+    private readonly IStackDataManager _stackDataManager;
+    private readonly IStudyAreaDataManager _studyAreaDataManager;
+
+    public TableVisualizationEngine(
+        IFlashCardDataManager flashCardDataManager,
+        IStackDataManager stackDataManager,
+        IStudyAreaDataManager studyAreaDataManager)
+    {
+        _flashCardDataManager = flashCardDataManager;
+        _stackDataManager = stackDataManager;
+        _studyAreaDataManager = studyAreaDataManager;
+    }
     public void ViewStacks()
     {
         Console.Clear();
-        
+
         ConsoleTableBuilder
-            .From(DbManager.GetStacks())
+            .From(_stackDataManager.GetStacks())
             .WithColumn("Lessons")
             .ExportAndWriteLine();
-        
+
         Console.WriteLine();
     }
 
     public void ViewFlashCards(Stack stack)
     {
         Console.Clear();
-        
+
         ConsoleTableBuilder
-            .From(DbManager.GetFlashCardsOfStack(stack))
+            .From(_flashCardDataManager.GetFlashCardsOfStack(stack))
             .WithTitle(stack.Name)
             .ExportAndWriteLine();
-        
+
         Console.WriteLine();
     }
 
     public void ViewHistory()
     {
         Console.Clear();
-        
-        ConsoleTableBuilder.From(DbManager.GetScoresHistory()).ExportAndWriteLine();
+
+        ConsoleTableBuilder.From(_studyAreaDataManager.GetScoresHistory()).ExportAndWriteLine();
         Console.WriteLine();
     }
 }
