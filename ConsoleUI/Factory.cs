b2383ea0d4691fbfaedfd10cb80c68ebd42bf916
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-10-15 23:20:40 +0300

    Implement factory pattern as ioc container

---
+++ ConsoleUI/Factory.cs
@@ -0,0 +1,86 @@
+using BusinessLogic.Input;
+using BusinessLogic.Services;
+using BusinessLogic.Services.Implementation;
+using BusinessLogic.TableVisualizer;
+using ConsoleUI.UI;
+using DataAccess.Config;
+using DataAccess.Data;
+using DataAccess.Data.Implementation;
+using DataAccess.DTO;
+
+namespace ConsoleUI;
+
+public static class Factory
+{
+    public static Configuration CreateConfiguration()
+    {
+        return new Configuration();
+    }
+
+    public static IStackDataManager CreateStackDataManager()
+    {
+        return new StackDataManager(CreateConfiguration());
+    }
+
+    public static IFlashCardDataManager CreateFlashCardDataManager()
+    {
+        return new FlashCardDataManager(
+            CreateConfiguration(),
+        CreateStackDataManager());
+    }
+
+    public static IStudyAreaDataManager CreateStudyAreaDataManager()
+    {
+        return new StudyAreaDataManager(
+            CreateConfiguration(),
+            CreateStackDataManager());
+    }
+
+    public static VisualizationService<TModel>
+    CreateVisualizationService<TModel>() where TModel : class
+    {
+        return new VisualizationService<TModel>();
+    }
+
+    public static UserInput CreateUserInput()
+    {
+        return new UserInput();
+    }
+
+    public static IFlashCardService CreateFlashCardService()
+    {
+        return new FlashCardService(
+            CreateUserInput(),
+            CreateVisualizationService<FlashCardDTO>(),
+            CreateFlashCardDataManager());
+    }
+
+    public static IStackService CreateStackService()
+    {
+        return new StackService(
+            CreateUserInput(),
+            CreateStackDataManager(),
+            CreateVisualizationService<StackDTO>(),
+            CreateFlashCardService());
+    }
+
+    public static IStudyAreaService CreateStudyAreaService()
+    {
+        return new StudyAreaService(
+            CreateUserInput(),
+            CreateVisualizationService<StudyAreaDto>(),
+            CreateFlashCardDataManager(),
+            CreateStudyAreaDataManager(),
+            CreateVisualizationService<StackDTO>(),
+            CreateStackDataManager());
+    }
+
+    public static ProgramController CreateProgramController()
+    {
+        return new ProgramController(
+            CreateUserInput(),
+            CreateFlashCardService(),
+            CreateStudyAreaService(),
+            CreateStackService());
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,28 +1,5 @@
-﻿using BusinessLogic.Input;
-using BusinessLogic.Services;
-using BusinessLogic.Services.Implementation;
-using BusinessLogic.TableVisualizer;
-using ConsoleUI.UI;
-using DataAccess.Config;
-using DataAccess.Data;
-using DataAccess.Data.Implementation;
-using DataAccess.DTO;
+﻿using ConsoleUI;
 
-
-Configuration config = new();
-
-IStackDataManager stackDataManager = new StackDataManager(config);
-IFlashCardDataManager flashCardDataManager = new FlashCardDataManager(config, stackDataManager);
-IStudyAreaDataManager studyAreaDataManager = new StudyAreaDataManager(config, stackDataManager);
-UserInput input = new();
-
-VisualizationService<FlashCardDTO> flashCardVisualizer = new();
-VisualizationService<StackDTO> stackVisualizer = new();
-VisualizationService<StudyAreaDto> studyAreaVisualizer = new();
-IFlashCardService flashCardService = new FlashCardService(input, flashCardVisualizer, flashCardDataManager);
-IStackService stackService = new StackService(input, stackDataManager, stackVisualizer, flashCardService);
-IStudyAreaService studyAreaService = new StudyAreaService(input, studyAreaVisualizer, flashCardDataManager, studyAreaDataManager, stackVisualizer, stackDataManager);
-
-ProgramController programController = new(input, flashCardService, studyAreaService, stackService);
+var programController = Factory.CreateProgramController();
 
 programController.StartProgram();
