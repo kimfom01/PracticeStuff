1d4ff245a7ab993fc606f2255d4bb20bd3eb3ba8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-11-15 10:05:29 +0300

    Update factory with new repositories

--- ConsoleUI/Factory.cs
+++ ConsoleUI/Factory.cs
@@ -4,74 +4,74 @@ using BusinessLogic.Services.Implementation;
 using BusinessLogic.TableVisualizer;
 using ConsoleUI.UI;
 using DataAccess.Config;
-using DataAccess.Data;
-using DataAccess.Data.Implementation;
-using DataAccess.DTO;
+using DataAccess.Dtos;
+using DataAccess.Repositories;
+using DataAccess.Repositories.Implementation;
 
 namespace ConsoleUI;
 
 public static class Factory
 {
-    public static Configuration CreateConfiguration()
+    private static Configuration CreateConfiguration()
     {
         return new Configuration();
     }
 
-    public static IStackDataManager CreateStackDataManager()
+    private static IStackRepository CreateStackDataManager()
     {
-        return new StackDataManager(CreateConfiguration());
+        return new StackRepository(CreateConfiguration());
     }
 
-    public static IFlashCardDataManager CreateFlashCardDataManager()
+    private static IFlashCardRepository CreateFlashCardDataManager()
     {
-        return new FlashCardDataManager(
+        return new FlashCardRepository(
             CreateConfiguration(),
         CreateStackDataManager());
     }
 
-    public static IStudyAreaDataManager CreateStudyAreaDataManager()
+    private static IStudyAreaRepository CreateStudyAreaDataManager()
     {
-        return new StudyAreaDataManager(
+        return new StudyAreaRepository(
             CreateConfiguration(),
             CreateStackDataManager());
     }
 
-    public static VisualizationService<TModel>
+    private static VisualizationService<TModel>
     CreateVisualizationService<TModel>() where TModel : class
     {
         return new VisualizationService<TModel>();
     }
 
-    public static UserInput CreateUserInput()
+    private static UserInput CreateUserInput()
     {
         return new UserInput();
     }
 
-    public static IFlashCardService CreateFlashCardService()
+    private static IFlashCardService CreateFlashCardService()
     {
         return new FlashCardService(
             CreateUserInput(),
-            CreateVisualizationService<FlashCardDTO>(),
+            CreateVisualizationService<FlashCardDto>(),
             CreateFlashCardDataManager());
     }
 
-    public static IStackService CreateStackService()
+    private static IStackService CreateStackService()
     {
         return new StackService(
             CreateUserInput(),
             CreateStackDataManager(),
-            CreateVisualizationService<StackDTO>(),
+            CreateVisualizationService<StackDto>(),
             CreateFlashCardService());
     }
 
-    public static IStudyAreaService CreateStudyAreaService()
+    private static IStudyAreaService CreateStudyAreaService()
     {
         return new StudyAreaService(
             CreateUserInput(),
             CreateVisualizationService<StudyAreaDto>(),
             CreateFlashCardDataManager(),
             CreateStudyAreaDataManager(),
-            CreateVisualizationService<StackDTO>(),
+            CreateVisualizationService<StackDto>(),
             CreateStackDataManager());
     }
 
