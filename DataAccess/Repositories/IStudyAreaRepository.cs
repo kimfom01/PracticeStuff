fb57f5a5a5e0e6d70e10a8edd352b28bbc107c26
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-11-15 23:19:37 +0300

    Update repositories to ef core

--- DataAccess/Repositories/IStudyAreaRepository.cs
+++ DataAccess/Repositories/IStudyAreaRepository.cs
@@ -1,11 +1,7 @@
-using DataAccess.Dtos;
 using DataAccess.Models;
 
 namespace DataAccess.Repositories;
 
-public interface IStudyAreaRepository
+public interface IStudyAreaRepository : IRepositoryBase<StudyArea>
 {
-    public Task CreateStudyAreaTable();
-    public Task SaveScore(StudyArea studyArea, Stack stack);
-    public Task<IEnumerable<StudyAreaDto>> GetScoresHistory();
 }
