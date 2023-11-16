b7cd7b3e59df55b02585bfad29bf8392e411ee2e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-11-17 01:16:46 +0300

    Update properties with clear name

--- DataAccess/Models/FlashCard.cs
+++ DataAccess/Models/FlashCard.cs
@@ -3,8 +3,8 @@ namespace DataAccess.Models;
 public class FlashCard
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Content { get; set; }
+    public required string Front { get; set; }
+    public required string Back { get; set; }
     
     public int StackId { get; set; }
     public Stack? Stack { get; set; }
